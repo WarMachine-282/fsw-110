277eda75b00baf6b72a5a301a6c51e4f74d55c24
Christian Van Loon <66512995+WarMachine-282@users.noreply.github.com>
2020-08-20 22:50:08 -0700

    first commit

---
+++ week4/dj_js/index.js
@@ -0,0 +1,7 @@
+var box = document.createElement('div');
+box.style.backgroundColor = "black"
+box.style.width = "200px";
+box.style.height = "200px";
+box.style.margin = "200px auto";
+document.body.append(box);
+
