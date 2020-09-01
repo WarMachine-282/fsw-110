2a1387db2bad2cddd5091d5c20c332c9475d1240
Christian Van Loon <66512995+WarMachine-282@users.noreply.github.com>
2020-08-31 18:47:22 -0700

    update

--- week5/submit_events/index.js
+++ week5/submit_events/index.js
@@ -1,10 +1,8 @@
-document.signUp.addEventListener("submit", function(d){
-        d.preventDefault()
-
-        var username = document.signUp.username.value
-        var email = document.signUp.email.value
-        var password = document.signUp.password.value
-
+document.getElementById("submit").addEventListener("click", () => {
+        var username = document.getElementById("username").value, 
+        email = document.getElementById("email").value, 
+        password = document.getElementById("password").value;
+        
         alert(` Sign up initiated!\n ${username} taken.\n ${email} available.\n ${password} not compatible.`)
 })
 
@@ -14,16 +12,3 @@ document.signUp.addEventListener("submit", function(d){
 
 
 
-
-// const myForm = document.getElementById("signUp");
-// const submitBtn = document.getElementById("submit");
-// submitBtn.addEventListener("click", message)
-
-
-// function message(){
-//     let username = myForm.elements["Username"].value;
-//     let email = myForm.elements["Email"].value;
-//     let password = myForm.elements["Password"].value;
-    
-//     alert(`Hello ${username} ${email}!\nSo your favorite language is: ${password} congrats!\nOh good, you would like to learn  next!`)    
-// }
